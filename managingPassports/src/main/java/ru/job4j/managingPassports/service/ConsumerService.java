kalenikov/job4j_managing_passports

2. Асинхронное взаимодействие [#458497]

src/main/java/ru/job4j/ManagingPassports/example/ConsumerService.java
@@ -1,0 +1,16 @@
+package ru.job4j.managingpassports.example;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import ru.job4j.managingpassports.model.Passport;
+
+@Service
+public class ConsumerService {
+    @KafkaListener(topics = "passport")
+    @Payload(required = false)
+    public void printMessage(ConsumerRecord<String, Passport> input) {
+        System.out.println(input.value());
+    }
+}

dmitriyermoshin19/job4j_managingPassports

managingPassports/src/main/java/ru/job4j/managingPassports/service/ConsumerService.java
@@ -1,0 +1,16 @@
+package ru.job4j.managingPassports.service;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import ru.job4j.managingPassports.model.Passport;
+
+@Service
+public class ConsumerService {
+    @KafkaListener(topics = "passport")
+    @Payload(required = false)
+    public void printMessage(ConsumerRecord<String, Passport> input) {
+        System.out.println(input.value());
+    }
+}